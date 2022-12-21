9cab60ab7b6a1eb48e75f28561f865529228007c
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2022-12-21 21:56:29 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -1,5 +1,5 @@
 name = "Armaan Singh Brar"
 SID = "22104024"
 Department_name = "Electrical Engineering"
-CGPA = "10"
+CGPA = "9.9"
 print(f"Hey, {name} Here! \nMy SID is {SID} \nI am from {Department_name} department and my CGPA is {CGPA}")
