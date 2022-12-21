8e7362f39683a9579488c5bb59a9c9488a8bb487
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2022-12-21 21:54:48 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,5 @@
+name = "Armaan Singh Brar"
+SID = "22104024"
+Department_name = "Electrical Engineering"
+CGPA = "10"
+print(f"Hey, {name} Here! \nMy SID is {SID} \nI am from {Department_name} department and my CGPA is {CGPA}")
